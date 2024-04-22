364d87d1f0b85c997f23cd09763423373e61456f
Brendon45 <jejebrendon722@gmail.com>
2024-04-22 13:36:06 -0700

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format
+   Records all tasks from all employees
+   Format must be: { "USER_ID": [ {"username": "USERNAME",
+   "task": "TASK_TITLE",
+   "completed": TASK_COMPLETED_STATUS}, {"username": "USERNAME",
+   "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS}, ... ],
+   "USER_ID": [ {"username": "USERNAME", "task": "TASK_TITLE",
+   "completed": TASK_COMPLETED_STATUS},
+   {"username": "USERNAME", "task": "TASK_TITLE",
+   "completed": TASK_COMPLETED_STATUS}, ... ]}
+   File name must be: todo_all_employees.json
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    resp = requests.get(url)
+    Users = resp.json()
+
+    users_dict = {}
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
+        url = url + '/todos/'
+        resp = requests.get(url)
+
+        tasks = resp.json()
+        users_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            users_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
+            })
+            """A little Something"""
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_dict, f)
