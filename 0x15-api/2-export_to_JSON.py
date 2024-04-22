fbca747dfb7930686fbd85ef8e56cc155925cfd3
Brendon45 <jejebrendon722@gmail.com>
2024-04-22 10:00:59 -0700

    Python script to export data in the JSON format.

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Python to get data from an API and convert to Json
     Records all tasks that are owned by this employee
-    Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+    Format must be: { "USER_ID": [{"task": "TASK_TITLE",
+    "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
+    {"task": "TASK_TITLE", "completed":
+    TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
     File name must be: USER_ID.json
 """
 import csv
