e809058491238cbb740f8f85bfd8ac7a9cd5ae47
Brendon45 <jejebrendon722@gmail.com>
2024-05-07 21:21:58 +0200

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-This module defines a Python script that queries the Reddit API
-to retrieve the number of subscribers for a given subreddit.
-"""
-
+"""Module defines a Python script that queries the Reddit API
+to retrieve the number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
+    Queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
-        int: Number of subscribers for the subreddit,
-             or 0 if the subreddit is invalid.
+        int: Number of subscribers or 0 if subreddit is invalid.
     """
-	# Define the User-Agent header for the Reddit API request
-	headers = {'User-Agent': 'Reddit API'}
+    headers = {'User-Agent': 'Reddit API'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-	# Construct the URL for the subreddit's information endpoint
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-	# Send an HTTP GET request to the Reddit API with custom headers
-	response = requests.get(url, headers=headers, allow_redirects=False)
-
-	# Check if the request was successful (HTTP status code 200)
-	if response.status_code == 200:
-		# Parse the JSON response and extract the number of subscribers
-		return response.json().get("data").get("subscribers")
-
-	# Return 0 if the subreddit is invalid or the request failed
-	return 0
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    return 0
